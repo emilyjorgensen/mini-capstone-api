ae97e2e7c7ea73e49adf1077403d3292fa6db9e9
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-10-24 11:53:51 -0500

    json rendered for one product and all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render :index
+  end
+
+  def one_product
+    @product = Product.first
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/all_products", controller: "products", action: "all_products"
+  get "/one_product", controller: "products", action: "one_product"
 end
