12abed8f93d641cdfc5ef63b3900d8b5ba2e3439
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-10-25 13:00:24 -0500

    added create request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: "peppermint tea",
+      price: 6,
+      image_url: nil,
+      description: "A cozy tea for a cozy book!",
+    )
+    render :show
+  end
 end
