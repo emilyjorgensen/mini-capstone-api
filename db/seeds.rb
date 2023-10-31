0803886af8c43bf8284cecd91ada8c85f47c1ed4
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-10-31 13:14:46 -0500

    add image jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,10 @@
 #   {name: "cheaters", price: "18.0", description: "Sometimes you need a little help to see the words. We've got you covered!", inventory: nil, supplier_id: 1}
 # ])
 
-Image.create(url: "https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8am91cm5hbHxlbnwwfHwwfHx8MA%3D%3D", product_id: 2)
-Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ-nEpo8yPzs58mhHwy_E-6WngTrGFHA9-PQ&usqp=CAU", product_id: 5)
-Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvEHBok3IBkG1M4hrgPVvIQyWuUjD22ONG-gfREXY5hqSgnoHwJUgqvmr124z-vCyA4E&usqp=CAU", product_id: 1)
-Image.create(url: "https://www.blockbluelight.com/cdn/shop/products/custom_resized_f36d554d-22ea-4bcd-9d81-fa4983b399f1_1_672x.jpg?v=1695097387", product_id: 3)
-Image.create(url: "https://www.cheatersreadingglasses.com/wp-content/uploads/2014/04/MV.Style67Black.jpg", product_id: 4)
+# Image.create(url: "https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8am91cm5hbHxlbnwwfHwwfHx8MA%3D%3D", product_id: 2)
+# Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ-nEpo8yPzs58mhHwy_E-6WngTrGFHA9-PQ&usqp=CAU", product_id: 5)
+# Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvEHBok3IBkG1M4hrgPVvIQyWuUjD22ONG-gfREXY5hqSgnoHwJUgqvmr124z-vCyA4E&usqp=CAU", product_id: 1)
+# Image.create(url: "https://www.blockbluelight.com/cdn/shop/products/custom_resized_f36d554d-22ea-4bcd-9d81-fa4983b399f1_1_672x.jpg?v=1695097387", product_id: 3)
+# Image.create(url: "https://www.cheatersreadingglasses.com/wp-content/uploads/2014/04/MV.Style67Black.jpg", product_id: 4)
+
+Image.create(url: "https://m.media-amazon.com/images/I/71BY5X9RDfL._AC_UF1000,1000_QL80_.jpg", product_id: 4)
