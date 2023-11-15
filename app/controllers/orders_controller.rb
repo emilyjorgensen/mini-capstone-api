2255dadaa57c15eb85ab408df5335e47e4e4e419
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-15 14:30:46 -0600

    calculated total, authorized show and index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,33 @@
 class OrdersController < ApplicationController
   def create
-    @order = Order.create(
-      user_id: params[:user_id],
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    @order = Order.new(
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     if @order.save
-      render json: { message: "Order complete." }
+      render :show
     else
       render json: { errors: @order.errors.full_messages }
     end
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    @order = current_user.order.find_by(id: params[:id])
     render :show
+  end
+
+  def index
+    @orders = current_user.orders
+    render :index
   end
 end
