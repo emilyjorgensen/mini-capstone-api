b16981a628fe999936cdb01338f7abef7d64c611
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-29 12:50:43 -0600

    update create action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,11 +1,13 @@
 class CartedProductsController < ApplicationController
+  before_action :authenticate_user
+
   def create
     @carted_product = CartedProduct.create(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
-      order_id: params[:order_id],
+      status: "carted",
+      order_id: nil,
     )
     if @carted_product.valid? #happy path
       render json: { message: "product(s) added to cart" }
