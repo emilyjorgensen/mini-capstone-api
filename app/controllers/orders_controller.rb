0ecd9726399e6e700a4a94dd75c30f49a584a385
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-29 15:17:13 -0600

    update order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    product = Product.find_by(id: params[:product_id])
+    carted_products = current_user.carted_products.where(status: "carted")
 
-    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.product.price * carted_product.quantity
+    end
+
     calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
 
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total,
     )
     if @order.save
+      carted_products.update_all(status: "purchased", order_id: @order.id)
       render :show
     else
       render json: { errors: @order.errors.full_messages }
